9141b57d1f1a139fb5ea2689d119cd1fb7cb8367
Helyosis <alois56200@gmail.com>
2020-05-02 10:01:12 +0200

    Added collision and gravity. Improver movement system

--- player.py
+++ player.py
@@ -1,18 +1,104 @@
 import pygame
+import math
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.x = x
-        self.y = y
+    def __init__(self, x, y, colliding_with):
+        super().__init__()
 
+        self.WIDTH = 50
+        self.HEIGHT = 70
+
+        self.speed = 5
+        self.jump_initial_speed = 15
+        self.jump_time = 0
+        self.MAX_JUMP_TIME = 20
+        self.keys_pressed = []
+        self.direction = 0
+
+
+        self.dx = self.dy = 0
+        self.MAX_DX = 5
+
+        #self.image = pygame.image.load("player.png").convert_alpha()
+        
         self.image = pygame.image.load("player.png").convert_alpha()
-        self.rect = self.image.get_rect(left=self.x, bottom=self.y)
+        self.image = pygame.transform.scale(self.image, (self.WIDTH, self.HEIGHT))
+        self.mask = pygame.mask.from_surface(self.image)
+        self.image.set_colorkey((0, 0, 0))
 
-    
+
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
+
+        self.colliding_with = colliding_with
+
+
     def move(self, deltax, deltay):
-        self.x += deltax
-        self.y += deltay
+        self.move_one_axis(deltax, 0)
+        self.move_one_axis(0, deltay)
 
+    def move_one_axis(self, deltax, deltay):
+        if deltax != 0:
+            self.rect.x += min(self.MAX_DX, max(-self.MAX_DX, deltax))
+            self._handle_collision(deltax, deltay)
+
+        if deltay != 0:
+            self.rect.y += deltay
+            self._handle_collision(deltax, deltay)
+
+
+    def _handle_movement(self):
+        keys = pygame.key.get_pressed()
+        new_keys = [k for k, state in enumerate(keys) if k not in self.keys_pressed and state]
+
+        if pygame.K_UP in new_keys:
+            self.jump(self.jump_initial_speed)
+        
+        
+        if pygame.K_DOWN in new_keys:
+            pass
+        
+        if pygame.K_RIGHT in new_keys or (keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]):
+            self.direction = 1
+        elif pygame.K_LEFT in new_keys or (keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]):
+            self.direction = -1
+       
+        if not keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
+           self.direction = 0
+
+        self.dx += self.speed * self.direction
+
+        self.keys_pressed = [k for k, state in enumerate(keys) if state]
+
+        
+
+    def _handle_collision(self, dx, dy):
+        colliding_sprites = pygame.sprite.spritecollide(self, self.colliding_with, False, pygame.sprite.collide_mask)
+        for sprite in colliding_sprites:
+            if dx > 0 : # Moving right; Hit the left side of the sprite
+                self.rect.right = sprite.rect.left
+                self.dx = 0
+            if dx < 0: # Moving left; Hit the right side of the sprite
+                self.rect.left = sprite.rect.right
+                self.dx = 0
+            if dy > 0: # Moving down; Hit the top side of the sprite
+                self.rect.bottom = sprite.rect.top
+                self.dy = 0
+                self.dx = int(self.dx / 2)
+                self.jump_time = 0
+            if dy < 0: # Moving up; Hit the bottom side of the sprite
+                self.rect.top = sprite.rect.bottom
+                self.dy = 0
+        
+    def _apply_gravity(self):
+        self.dy += 1
+
+    def jump(self, force):
+        if self.jump_time == 0:
+            self.dy -= force
+            self.jump_time += 1
     def update(self):
-        pass
+        self._handle_movement()
+        self._apply_gravity()
+
+        self.move(self.dx, self.dy)
--- screen.py
+++ screen.py
@@ -2,24 +2,30 @@ import pygame
 pygame.init()
 
 from player import Player
+from floor import Floor
 
 
 launched = True
-width, height = 800, 600
+width, height = 1000, 800
 
 screen = pygame.display.set_mode((width, height))
 
 all_sprites = pygame.sprite.Group()
+collide_with_player = pygame.sprite.Group()
 clock = pygame.time.Clock()
 
-player = Player(50, 50)
-all_sprites.add(player)
+player_character = Player(50, 50, collide_with_player)
+all_sprites.add(player_character)
+
+floor = Floor(x = 0, y = 300)
+all_sprites.add(floor)
+collide_with_player.add(floor)
 
 while launched:
-    for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                launched=False
+    if pygame.event.get(pygame.QUIT):
+        launched=False
 
+    #Game logic
     all_sprites.update()
 
     #Draw background
@@ -31,3 +37,5 @@ while launched:
     #Refresh display and set FPS to 60
     pygame.display.flip()
     clock.tick(60)
+
+pygame.quit()
